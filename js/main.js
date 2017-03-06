f43a6683f5d73de3916c5e0adae9f5db8782e92f
maia rojas <maia.rt.46@gmail.com>
2017-03-06 01:38:39 -0500

    Se mejoro el html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	
+ var name = document.getElementById("name").value;
+ var lastname = document.getElementById("lastname").value;
+ var password = document.getElementById("input-password").value;
+ var email = document.getElementById("input-email").value;
+ var opcion = document.getElementById("opcion").value;
+
+ var texto = /[A-Z]{1,}[a-z]+/ ;
+ var correo =/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]$/ ;
+
+if((name != "" && lastname  != "") && (password != "" && opcion != 0)){
+  if(!name.match(texto)){
+    alert("The first letter of the name must be in uppercase");
+  }
+
+  else if(!lastname.match(texto)){
+    alert(" The first letter of the last name must be in uppercase");
+  }
+
+  else if(!email.match(correo)){
+    alert("Enter an invalid email: \n Sign in xxxx@yyyyy.zzzz");
+  }
+
+  else if(password.length < 7 || password == "password" || password == "123456" || password == "098754"){
+    alert("Enter a password with digits greater than 6, \n it should not be 123456 \n or 098754");
+  }
+  else{
+  alert("Congratulations, you got to go .....!!!!");}
+
+}
+else
+	{alert("Enter all required data");}
+
+
 }
